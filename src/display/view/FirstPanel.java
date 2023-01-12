2abc23e89c87d3a777249c346998df06a0d52376
darynamm <darynamanuilenko@gmail.com>
2023-01-12 09:50:34 -0700

    made it so that the button could change colors (updated setuplisteenrs)

--- src/display/view/FirstPanel.java
+++ src/display/view/FirstPanel.java
@@ -1,6 +1,7 @@
 package display.view;
 import display.controller.Controller;
 import javax.swing.JPanel;
+import javax.swing.JButton;
 import java.awt.Color;
 
 
@@ -8,11 +9,13 @@ public class FirstPanel extends JPanel
 {
 	
 private Controller app;
+private JButton colorButton;
 
 public FirstPanel(Controller app)
 {
 	super();
 	this.app = app;
+	this.colorButton = new JButton("Click to change color");
 	
 	setupPanel();
 	setupListeners();
@@ -22,16 +25,34 @@ public FirstPanel(Controller app)
 private void setupPanel()
 {
 	this.setBackground(Color.MAGENTA);
+	this.add(colorButton);
 }
 
 
 private void setupListeners()
 {
-	
+	colorButton.addActionListener(click -> updateBackground());
 }
 
 private void setupLayout()
 {
 	
+}
+
+private void updateBackground()
+{
+	this.setBackground(generateRandomColor());
+}
+private Color generateRandomColor()
+{
+	Color randomColor;
+	
+	int red = (int) (Math.random()* 256);
+	int green = (int) (Math.random() * 256);
+	int blue = (int) (Math.random() * 256);
+	
+	randomColor = new Color(red, green, blue);
+	
+	return randomColor;
 }
 }
