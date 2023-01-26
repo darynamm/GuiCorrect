e0871b52a7a86107dddb4de863d8f39a8d0b098f
darynamm <darynamanuilenko@gmail.com>
2023-01-26 08:11:25 -0700

    added jbutton

--- src/display/view/FirstPanel.java
+++ src/display/view/FirstPanel.java
@@ -10,12 +10,13 @@ public class FirstPanel extends JPanel
 	
 private Controller app;
 private JButton colorButton;
-
+private JButton coolButton;
 public FirstPanel(Controller app)
 {
 	super();
 	this.app = app;
 	this.colorButton = new JButton("Click to change color");
+	this.coolButton = new JButton("Click");
 	
 	setupPanel();
 	setupListeners();
@@ -26,16 +27,20 @@ private void setupPanel()
 {
 	this.setBackground(Color.MAGENTA);
 	this.add(colorButton);
+	this.add(coolButton);
 }
 
 
 private void setupListeners()
 {
 	colorButton.addActionListener(click -> updateBackground());
+	coolButton.addActionListener(click -> updateBackground());
+
 }
 
 private void setupLayout()
 {
+	//layout.putConstraint(Spring.layout.WEST, colorButton, 10, SpringLayout.WEST, this);
 	
 }
 
